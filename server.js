af9f64f204a8063e6906f2f2e0afcacf1492923d
CrisKop <62496847+CrisKop@users.noreply.github.com>
2023-06-04 15:48:08 -0500

    Refresh

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
 app.use(express.static('public'))
 
 app.use(express.json({limit: '50mb'}));
@@ -11,7 +11,7 @@ app.use(express.urlencoded({limit: '50mb'}));
 // use res.render to load up an ejs view file
 
 app.get('/', function(req, res) {
-    res.render('pages/index')
+    res.render('index')
 })
 
 app.listen(1000);
